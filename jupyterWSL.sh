5c602d338286be80227da80cef009e7c9ed2536d
José Costa <josepirescosta44.jc68@gmail.com>
2024-04-03 18:36:13 +0100

    Update jupyterWSL.sh

--- jupyterWSL.sh
+++ jupyterWSL.sh
@@ -3,59 +3,56 @@
 # Colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW="\033[0;33m"
+CYAN="\033[0;36m"
 NC='\033[0m' # No Color
 
 # Run update and upgrade
-echo -e "${GREEN}Running sudo apt update && upgrade...${NC}"
+echo -e "${YELLOW}Running sudo apt update && upgrade...${NC}"
 sudo apt update && sudo apt upgrade
 
-# Install necessary packages
-echo "Installing python3, python3-pip, and ipython3..."
-sudo apt install python3 python3-pip ipython3
-
-# Install python3-pip if not installed already
-echo "Installing python3-pip if not installed..."
-sudo apt install python3-pip
-
 # Check if python3 is installed
-echo -e "${GREEN}Checking if python3 is installed...${NC}"
+echo -e "${CYAN}Checking if python3 is installed...${NC}"
 if ! command -v python3 &> /dev/null; then
-    echo -e "${RED}python3 is not installed. Aborting script.${NC}"
-    exit 1
+    echo "${YELLOW}Installing python3...${NC}"
+    sudo apt install python3
+else
+    echo -e "${GREEN}python3 is already installed.${NC}"
 fi
 
 # Check if python3-pip is installed
-echo -e "${GREEN}Checking if python3-pip is installed...${NC}"
+echo -e "${CYAN}Checking if python3-pip is installed...${NC}"
 if ! command -v pip3 &> /dev/null; then
-    echo -e "${RED}python3-pip is not installed. Aborting script.${NC}"
-    exit 1
+    echo "${YELLOW}Installing python3-pip...${NC}"
+    sudo apt install python3-pip
+else
+    echo -e "${GREEN}python3-pip is already installed.${NC}"
 fi
 
 # Check if ipython3 is installed
-echo -e "${GREEN}Checking if ipython3 is installed...${NC}"
+echo -e "${CYAN}Checking if ipython3 is installed...${NC}"
 if ! command -v ipython3 &> /dev/null; then
-    echo -e "${RED}ipython3 is not installed. Aborting script.${NC}"
-    exit 1
+    echo "${YELLOW}Installing ipython3...${NC}"
+    sudo apt install ipython3
+else
+    echo -e "${GREEN}ipython3 is already installed.${NC}"
 fi
-
-# Install Jupyter
-echo -e "${GREEN}Installing Jupyter...${NC}"
-sudo pip3 install jupyter
 
 # Check if Jupyter is installed
-echo -e "${GREEN}Checking if Jupyter is installed...${NC}"
+echo -e "${CYAN}Checking if Jupyter is installed...${NC}"
 if ! command -v jupyter &> /dev/null; then
-    echo -e "${RED}Jupyter is not installed. Aborting script.${NC}"
-    exit 1
+    echo "${YELLOW}Installing Jupyter...${NC}"
+    sudo pip3 install jupyter
+else
+    echo -e "${GREEN}Jupyter is already installed.${NC}"
 fi
-echo -e "${GREEN}Jupyter is installed!!${NC}"
 
 # Install requirements.txt if it exists
 if [ -f "requirements.txt" ]; then
-    echo -e "${GREEN}Installing packages from requirements.txt...${NC}"
+    echo -e "${YELLOW}Installing packages from requirements.txt...${NC}"
     sudo pip3 install -r requirements.txt
 
-    echo -e "${GREEN}Checking if packages were installed successfully...${NC}"
+    echo -e "${CYAN}Checking if packages were installed successfully...${NC}"
     while IFS= read -r package
     do
         if ! pip3 freeze | grep -q "$package"; then
